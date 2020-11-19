4721bc17da0acf6707d563d1c80de7dd8b56f26c
Will Andre <wandre94@gmail.com>
2020-11-19 16:18:34 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -3,8 +3,8 @@ require_relative '../conversions.rb'
 describe "conversions" do
   describe '#ounces_to_grams' do
     it 'given zero, returns 0.0' do
-      grams = ounces_to_grams(0)
-      expect(grams).to eq(0.0)
+      grams = ounces_to_grams(1)
+      expect(grams).to eq(28.3495)
     end
   end
 end
